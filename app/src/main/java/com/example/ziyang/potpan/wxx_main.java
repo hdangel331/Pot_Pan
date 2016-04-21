a3b50ac27705111d9a8f05fe95270c807a2ce840
hhhhhh <jingye.hong13@xjtlu.edu.cn>
2016-04-22 06:05:40 +0800

    lib
    lib

--- app/src/main/java/com/example/ziyang/potpan/wxx_main.java
+++ app/src/main/java/com/example/ziyang/potpan/wxx_main.java
@@ -1,14 +1,21 @@
 package com.example.ziyang.potpan;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,14 +32,21 @@ public class wxx_main extends Activity {
     private ListView mDrawerList;
     private ArrayList<String> menuLists;
     private ArrayAdapter<String> adapter;
-
+    private Context mContext = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.wxx_main);
-
+        mContext = this;
+        ImageButton button = (ImageButton) findViewById(R.id.imageButtonLast);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                showPopupWindow(view);
+            }
+        });
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.right_drawer);
 
@@ -68,17 +82,45 @@ public class wxx_main extends Activity {
             }
         });
 
+//
+//        imageButtonlast = (ImageButton) findViewById(R.id.imageButtonLast);
+//        imageButtonlast.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setClass(wxx_main.this, hjy_main.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        imageButtonlast = (ImageButton) findViewById(R.id.imageButtonLast);
-        imageButtonlast.setOnClickListener(new View.OnClickListener() {
+
+    }
+    private void showPopupWindow(View view) {
+        View contentView = LayoutInflater.from(mContext).inflate(R.layout.hjy_popuplayout, null);
+        Button button = (Button) contentView.findViewById(R.id.edt);
+        Button thebutton = (Button) contentView.findViewById(R.id.lib);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent();
                 intent.setClass(wxx_main.this, hjy_main.class);
                 startActivity(intent);
             }
         });
+        thebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(wxx_main.this, hjy_lib.class);
+                startActivity(intent);
+            }
+        });
+        final PopupWindow popupWindow = new PopupWindow(contentView, ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT,true);
+        popupWindow.setTouchable(true);
 
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        popupWindow.setOutsideTouchable(true);
 
+        popupWindow.showAtLocation(view, Gravity.CENTER,0 ,0);
     }
 }
