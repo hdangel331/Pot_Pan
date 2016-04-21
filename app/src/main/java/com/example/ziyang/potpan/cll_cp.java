d7f84c1a1dd09f8bdd6b0047daabdb5e62e8054d
cll <935618091@qq.com>
2016-04-22 00:35:25 +0800

    2COMMIT

---
+++ app/src/main/java/com/example/ziyang/potpan/cll_cp.java
@@ -0,0 +1,36 @@
+package com.example.ziyang.potpan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.ziyang.potpan.R;
+
+/**
+ * Created by CandiesCLL on 2016/4/19.
+ */
+public class cll_cp extends Activity {
+    private Button backbutton1;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cll_cp);
+
+        backbutton1 = (Button) findViewById(R.id.back1);
+        backbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(cll_cp.this, cll_main.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
