6996fce405ce83c2def56614502fd3d1caf8b6bf
Teresa12138 <532604824@qq.com>
2016-04-21 14:25:47 +0800

    first commit

--- app/src/main/java/com/example/ziyang/potpan/wxx_main.java
+++ app/src/main/java/com/example/ziyang/potpan/wxx_main.java
@@ -3,9 +3,14 @@ package com.example.ziyang.potpan;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ziyang on 2016/4/15.
@@ -14,12 +19,34 @@ public class wxx_main extends Activity {
 
     private Button button2;
     private ImageButton imageButtonlast;
+    private Button Button1;
+    private Button button3;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ArrayList<String> menuLists;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.wxx_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.right_drawer);
+
+
+        Button1 = (Button) findViewById(R.id.Button1);
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(wxx_main.this, zzy_main.class);
+                startActivity(intent);
+            }
+        });
+
 
         button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +58,17 @@ public class wxx_main extends Activity {
             }
         });
 
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(wxx_main.this, zzy_main.class);
+                startActivity(intent);
+            }
+        });
+
+
         imageButtonlast = (ImageButton) findViewById(R.id.imageButtonLast);
         imageButtonlast.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +78,6 @@ public class wxx_main extends Activity {
                 startActivity(intent);
             }
         });
-
 
 
     }
