415794e03cbe0111ba5e29252762e0af2df5de5e
zhangziyang13 <497388727@qq.com>
2016-04-24 18:04:53 +0800

    commit

---
+++ app/src/main/java/com/example/ziyang/potpan/zzy_constants.java
@@ -0,0 +1,39 @@
+package com.example.ziyang.potpan;
+
+/**
+ * Created by Ziyang on 2016/4/24.
+ */
+public final class zzy_constants {
+
+    public static final String[] IMAGES = new String[]{
+            "http://img.taopic.com/uploads/allimg/140720/240467-140H00K62786.jpg",
+            "http://image.tianjimedia.com/uploadImages/2012/345/44R6RY4Q0219_1680x1050.jpg",
+            "http://image.tianjimedia.com/uploadImages/2013/134/001GKNRJ7FCO_1440x900.jpg",
+            "http://image.tianjimedia.com/uploadImages/2011/360/89241H21087M.jpg",
+            "http://tupian.enterdesk.com/2014/lxy/2014/05/07/2/8.jpg",
+            "http://s9.knowsky.com/bizhi/l/1-5000/200952814217740135370.jpg",
+            "http://image.tianjimedia.com/uploadImages/2013/304/Y83292KWIFAT_1920x1200.jpg",
+            "http://tupian.enterdesk.com/2013/xll/003/0312/aodaliya/1.jpg",
+            "http://img.tuku.cn/file_big/201504/024448d7ae994d4d9273ea5f3c199a5a.jpg",
+            "http://p2.image.hiapk.com/uploads/allimg/130702/23-130F2135J5.jpg",
+
+            "http://5758566.51zfx.net/Admin/Uploads/2014/6/19/5758566/FCKeditorIMG/201406191056098007.png",
+            "http://cdn.pcbeta.attachment.inimc.com/data/attachment/forum/201207/06/193549jnr3nnp9mdjmzdvz.png",
+            "http://pic10.nipic.com/20101021/5252423_193715833000_2.png",
+            "http://www.zwyz.com/bbs/data/attachment/forum/201408/11/1728280fkfl55z372l307r.png",
+            "http://pic14.nipic.com/20110505/6645012_200821286159_2.png"
+    };
+
+    private zzy_constants(){
+
+    }
+
+
+    public static class Config {
+//        public static final boolean DEVELOPER_MODE = false;
+    }
+    public static class Extra {
+//        public static final String FRAGMENT_INDEX = "com.nostra13.example.universalimageloader.FRAGMENT_INDEX";
+//        public static final String IMAGE_POSITION = "com.nostra13.example.universalimageloader.IMAGE_POSITION";
+    }
+}
