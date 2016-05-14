9004bf27bfdf0d48fd66fd186337edf49c885df2
hhhhhh <jingye.hong13@xjtlu.edu.cn>
2016-05-14 21:06:16 +0800

    caicaicai

--- app/src/main/java/com/example/ziyang/potpan/hjy_main.java
+++ app/src/main/java/com/example/ziyang/potpan/hjy_main.java
@@ -1,43 +1,89 @@
 package com.example.ziyang.potpan;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Gallery;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 
-public class hjy_main extends Activity implements OnItemClickListener {
+
+public class hjy_main extends Activity  {
 
     private Gallery galleryM,galleryS ;
+    private EditText recipeName;
+    private TextView outputRecipeName, outputMaterials,outputSeasons;
+    private Context mContext = null;
+    private Button cancelRecipe, createRecipe;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         cll_exit.getInstance().addActivity(this);
         super .onCreate(savedInstanceState);
         setContentView(R.layout.hjy_main );
-        // 创建一个 ImageAdapter 对象
+        mContext = this;
         hjy_adapter adapter = new hjy_adapter( this );
         hjy_dadpter_seasons adapters = new hjy_dadpter_seasons(this);
-        // 获取在 main.xml 中定义的 Gallery 对象
+        final String materials[] = {
+                "bacon","balsam pear","beef","carrot","celery","checken","checken breast",
+                "chinese cabbage","corn","drumstick","egg","eggplant","fish","leek",
+                "lettuce","tomato","mushroom","potapo","pumpkin","rice","shrimp",};
+        final String seasons[] = {"diyigeX","diergeX","disangeX","disigeX","diwugeX","diliugeX","diqigeX","dibageX","dijiugeX"};
+        recipeName = (EditText) findViewById(R.id.editRecipeName);
+        outputRecipeName = (TextView) findViewById(R.id.outputName);
+        outputMaterials = (TextView) findViewById(R.id.outputMaterials);
+        outputSeasons = (TextView) findViewById(R.id.outputSeasons);
+        recipeName.setOnKeyListener(new EditText.OnKeyListener(){
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                outputRecipeName.setText(recipeName.getText().toString());
+                return false;
+            }
+        });
         galleryM = (Gallery) this .findViewById(R.id.galleryM );
         galleryS = (Gallery) this.findViewById(R.id.galleryS);
-        // 设定图片之间的间隔 ( 如果无下面的语句，有时候图片会重叠 )
-        galleryM.setSpacing(2);
-        galleryS.setSpacing(2);
-        // 将 adapter 和 gallery 关联起来
+        galleryM.setSpacing(6);
+        galleryS.setSpacing(6);
         galleryM.setAdapter(adapter);
         galleryS.setAdapter(adapters);
         galleryM.setSelection(4);
         galleryS.setSelection(4);
-        // 给 gallery 设定 OnItemClickListener
-        galleryM.setOnItemClickListener( this );
-        galleryS.setOnItemClickListener(this);
+        galleryM.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText ( mContext , "Position = " + position, Toast. LENGTH_SHORT ).show();
+                outputMaterials.append(materials[position]+"; ");
+            }
+        });
+        galleryS.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                outputSeasons.append(seasons[position]+"; ");
+            }
+        });
+        cancelRecipe = (Button) findViewById(R.id.cancelRecipe);
+        cancelRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        createRecipe = (Button) findViewById(R.id.createRecipe);
+        createRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "haha" , Toast. LENGTH_SHORT).show();
+            }
+        });
     }
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-    {
-        Toast.makeText ( this , "Position = " + position, Toast. LENGTH_SHORT ).show();
-    }
+
 }
