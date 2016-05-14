fcd92ef194aa20ac325bdc71c42f44ce2c4ddea8
Teresa12138 <532604824@qq.com>
2016-05-14 10:52:24 +0800

    commit5/14/01

--- app/src/main/java/com/example/ziyang/potpan/wxx_AboutUsActivity.java
+++ app/src/main/java/com/example/ziyang/potpan/wxx_AboutUsActivity.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Administrator on 2016/5/4.
+ * Created by wxx on 2016/5/4.
  */
 
 
@@ -61,91 +61,84 @@ public class wxx_AboutUsActivity extends Activity {
         viewList.add(view2);
         viewList.add(view3);
 
-//初始化指示器位置
-        initCursorPos();
-
-        viewPager.setAdapter(new MyPagerAdapter(viewList));
-        viewPager.setOnPageChangeListener(new MyPageChangeListener());
-
-    }
-
-
-    //初始化指示器位置
-    public void initCursorPos() {
-        // 初始化动画
-        cursor = (ImageView) findViewById(R.id.cursor);
-        bmpw = BitmapFactory.decodeResource(getResources(), R.drawable.wxx_a)
-                .getWidth();// 获取图片宽度
-
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int screenW = dm.widthPixels;// 获取分辨率宽度
-        offset = (screenW / viewList.size() - bmpw) / 2;// 计算偏移量
-
-        Matrix matrix = new Matrix();
-        matrix.postTranslate(offset, 0);
-        cursor.setImageMatrix(matrix);// 设置动画初始位置
-    }
+////初始化指示器位置
+//        initCursorPos();
+//
+//        viewPager.setAdapter(new MyPagerAdapter(viewList));
+//        viewPager.setOnPageChangeListener(new MyPageChangeListener());
+//
+//    }
 
 
+//    //初始化指示器位置
+//    public void initCursorPos() {
+//        // 初始化动画
+//        cursor = (ImageView) findViewById(R.id.cursor);
+//        bmpw = BitmapFactory.decodeResource(getResources(), R.drawable.wxx_a)
+//                .getWidth();// 获取图片宽度
+//
+//        DisplayMetrics dm = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        int screenW = dm.widthPixels;// 获取分辨率宽度
+//        offset = (screenW / viewList.size() - bmpw) / 2;// 计算偏移量
+//
+//        Matrix matrix = new Matrix();
+//        matrix.postTranslate(offset, 0);
+//        cursor.setImageMatrix(matrix);// 设置动画初始位置
+//    }
+//
+//
+//
+//    //页面改变监听器
+//    public class MyPageChangeListener implements ViewPager.OnPageChangeListener {
+//
+//        int one = offset * 2 + bmpw;// 页卡1 -> 页卡2 偏移量
+//        int two = one * 2;// 页卡1 -> 页卡3 偏移量
+//
+//        @Override
+//        public void onPageSelected(int arg0) {
+//            Animation animation = null;
+//            switch (arg0) {
+//                case 0:
+//                    if (currIndex == 1) {
+//                        animation = new TranslateAnimation(one, 0, 0, 0);
+//                    } else if (currIndex == 2) {
+//                        animation = new TranslateAnimation(two, 0, 0, 0);
+//                    }
+//                    break;
+//                case 1:
+//                    if (currIndex == 0) {
+//                        animation = new TranslateAnimation(offset, one, 0, 0);
+//                    } else if (currIndex == 2) {
+//                        animation = new TranslateAnimation(two, one, 0, 0);
+//                    }
+//                    break;
+//                case 2:
+//                    if (currIndex == 0) {
+//                        animation = new TranslateAnimation(offset, two, 0, 0);
+//                    } else if (currIndex == 1) {
+//                        animation = new TranslateAnimation(one, two, 0, 0);
+//                    }
+//                    break;
+//            }
+//            currIndex = arg0;
+//            animation.setFillAfter(true);// True:图片停在动画结束位置
+//            animation.setDuration(300);
+//            cursor.startAnimation(animation);
+//        }
+//
+//        @Override
+//        public void onPageScrolled(int arg0, float arg1, int arg2) {
+//        }
+//
+//        @Override
+//        public void onPageScrollStateChanged(int arg0) {
+//        }
+//    }
 
-    //页面改变监听器
-    public class MyPageChangeListener implements ViewPager.OnPageChangeListener {
-
-        int one = offset * 2 + bmpw;// 页卡1 -> 页卡2 偏移量
-        int two = one * 2;// 页卡1 -> 页卡3 偏移量
-
-        @Override
-        public void onPageSelected(int arg0) {
-            Animation animation = null;
-            switch (arg0) {
-                case 0:
-                    if (currIndex == 1) {
-                        animation = new TranslateAnimation(one, 0, 0, 0);
-                    } else if (currIndex == 2) {
-                        animation = new TranslateAnimation(two, 0, 0, 0);
-                    }
-                    break;
-                case 1:
-                    if (currIndex == 0) {
-                        animation = new TranslateAnimation(offset, one, 0, 0);
-                    } else if (currIndex == 2) {
-                        animation = new TranslateAnimation(two, one, 0, 0);
-                    }
-                    break;
-                case 2:
-                    if (currIndex == 0) {
-                        animation = new TranslateAnimation(offset, two, 0, 0);
-                    } else if (currIndex == 1) {
-                        animation = new TranslateAnimation(one, two, 0, 0);
-                    }
-                    break;
-            }
-            currIndex = arg0;
-            animation.setFillAfter(true);// True:图片停在动画结束位置
-            animation.setDuration(300);
-            cursor.startAnimation(animation);
-        }
-
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-        }
-    }
 
 
-
-    public class MyPagerAdapter extends PagerAdapter {
-        public List<View> ListViews;
-
-        public MyPagerAdapter(List<View> ListViews) {
-            this.ListViews = ListViews;
-        }
-
-//        PagerAdapter pagerAdapter = new PagerAdapter() {
+        PagerAdapter pagerAdapter = new PagerAdapter() {
 
 
             @Override
@@ -200,11 +193,11 @@ public class wxx_AboutUsActivity extends Activity {
 
 
 
-    }
+    };
 
 
 
+        viewPager.setAdapter(pagerAdapter);
 
 
-
-}
+}}
