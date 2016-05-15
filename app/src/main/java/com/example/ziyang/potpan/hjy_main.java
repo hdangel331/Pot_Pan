561d38259b310fb7bb55304751820e0bf5004bc3
hhhhhh <jingye.hong13@xjtlu.edu.cn>
2016-05-15 19:56:29 +0800

    newnewnew

--- app/src/main/java/com/example/ziyang/potpan/hjy_main.java
+++ app/src/main/java/com/example/ziyang/potpan/hjy_main.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -44,11 +46,23 @@ public class hjy_main extends Activity  {
         outputRecipeName = (TextView) findViewById(R.id.outputName);
         outputMaterials = (TextView) findViewById(R.id.outputMaterials);
         outputSeasons = (TextView) findViewById(R.id.outputSeasons);
-        recipeName.setOnKeyListener(new EditText.OnKeyListener(){
+//        recipeName.setOnKeyListener(new EditText.OnKeyListener(){
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event){
+//                outputRecipeName.setText(recipeName.getText().toString());
+//                return false;
+//            }
+//        });
+        recipeName.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event){
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
                 outputRecipeName.setText(recipeName.getText().toString());
-                return false;
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
         galleryM = (Gallery) this .findViewById(R.id.galleryM );
