3fa4dcd734d58aa48ee00887e497452c878d15ed
Teresa12138 <532604824@qq.com>
2016-05-18 14:40:07 +0800

    commit5/18/01

---
+++ app/src/main/java/com/example/ziyang/potpan/wxx_help3.java
@@ -0,0 +1,20 @@
+package com.example.ziyang.potpan;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/5/18.
+ */
+public class wxx_help3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View view = inflater.inflate(R.layout.wxx_help3, container, false);
+        return view;
+    }
+}
